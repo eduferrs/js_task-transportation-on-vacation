cbf4554ccf034c1dbe5020db7572f5e0750d3e15
eduferrs <edu.ferr96@gmail.com>
2026-02-10 16:25:46 -0300

    done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,20 @@
  *
  * @return {number}
  */
+
+const LONG_TERM = 7;
+const SHORT_TERM = 3;
+const LONG_TERM_DISCOUNT = 50;
+const SHORT_TERM_DISCOUNT = 20;
+
 function calculateRentalCost(days) {
-  // write code here
+  if (days >= LONG_TERM) {
+    return days * 40 - LONG_TERM_DISCOUNT;
+  } else if (days >= SHORT_TERM) {
+    return days * 40 - SHORT_TERM_DISCOUNT;
+  }
+
+  return days * 40;
 }
 
 module.exports = calculateRentalCost;
